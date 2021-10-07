9b9be3f4837bfe8653247da7c686757481b4e7a0
Mohsen Moradi <mohsen.moradi@capgemini.com>
2021-10-07 09:46:39 +0100

    add admin routes

---
+++ src/routes/admins/controllers.js
@@ -0,0 +1,9 @@
+export const getAdmin = async (req, res) => {
+  // const { userId } = req.params;
+  try {
+    return res.status(200).send({ user: "admin data" });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Admin not found");
+  }
+};
---
+++ src/routes/admins/routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getAdmin } from "./controllers";
+
+const router = express.Router();
+// localhost:3001/admins
+router.get("/:userId", getAdmin);
+
+export default router;
