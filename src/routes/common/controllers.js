61cf83582c52f8938f54f2414d6dd29d67e5b4a8
Nirmeet <nirmeet.baweja@gmail.com>
2021-10-08 16:31:43 +0100

    created some apis to test DB

--- src/routes/common/controllers.js
+++ src/routes/common/controllers.js
@@ -1,3 +1,5 @@
+const knex = require('../../knex')
+
 export const getListOfStudyGroups = async (req, res) => {
   try {
     return res.status(200).send([{ id: 'return list of HWC study group' }])
@@ -5,4 +7,37 @@ export const getListOfStudyGroups = async (req, res) => {
     console.log(err)
     return res.status(400).send('Error')
   }
+}
+
+export const getRoles = async (req, res) => {
+  const roles = await knex('roles').select('*').orderBy('id')
+  res.json(roles)
+}
+
+export const getUsers = async (req, res) => {
+  const users = await knex('users')
+    .select('firstname', 'last_name', 'email', 'role_id', 'cohort_id')
+    .orderBy('id')
+  res.json(users)
+}
+
+export const getVolunteers = async (req, res) => {
+  const volunteers = await knex('users')
+    .select('firstname', 'last_name', 'email')
+    .where('role_id', 2)
+    .orderBy('id')
+  res.json(volunteers)
+}
+
+export const getStudents = async (req, res) => {
+  const students = await knex('users')
+    .select('firstname', 'last_name', 'email', 'cohort_id')
+    .where('role_id', 3)
+    .orderBy('id')
+  res.json(students)
+}
+
+export const getClasses = async (req, res) => {
+  const classes = await knex('classes').select('*').orderBy('id')
+  res.json(classes)
 }
--- src/routes/common/routes.js
+++ src/routes/common/routes.js
@@ -1,8 +1,20 @@
 import express from 'express'
-import { getListOfStudyGroups } from './controllers'
+import {
+  getListOfStudyGroups,
+  getRoles,
+  getUsers,
+  getVolunteers,
+  getStudents,
+  getClasses,
+} from './controllers'
 
 const router = express.Router()
 // localhost:3001/data
+router.get('/roles', getRoles)
+router.get('/users', getUsers)
+router.get('/volunteers', getVolunteers)
+router.get('/students', getStudents)
+router.get('/classes', getClasses)
 router.get('/study-groups', getListOfStudyGroups)
 
 export default router
