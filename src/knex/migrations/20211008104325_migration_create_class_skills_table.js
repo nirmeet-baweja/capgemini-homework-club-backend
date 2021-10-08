3bf8bc5ed46e0b3a0093160f004a0684352b6c32
Nirmeet <nirmeet.baweja@gmail.com>
2021-10-08 12:28:17 +0100

    corrected the table name for class_skills

--- src/knex/migrations/20211008104325_migration_create_class_skills_table.js
+++ src/knex/migrations/20211008104325_migration_create_class_skills_table.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('user_skills', (table) => {
+  return knex.schema.createTable('class_skills', (table) => {
     table.primary(['class_id', 'skill_id'], 'id')
     table.integer('class_id').references('id').inTable('classes')
     table.integer('skill_id').references('id').inTable('skills')
@@ -7,5 +7,5 @@ exports.up = function (knex) {
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('user_skills')
+  return knex.schema.dropTable('class_skills')
 }
