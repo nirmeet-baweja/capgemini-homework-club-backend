2f5812f5cd37fca0d1e8e6687222a576d36133ef
Nirmeet <nirmeet.baweja@gmail.com>
2021-10-13 19:42:06 +0100

    deleted the unused development environment settings from knexfile.js
    Deleted the dev env settings that were used for connecting to DB stored locally.

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,21 @@
 require('dotenv').config()
 
 module.exports = {
-  // development: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     connectionString: process.env.DATABASE_URL,
-  //     connectionTimeoutMillis: 5000,
-  //     ssl: {
-  //       rejectUnauthorized: false,
-  //     },
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: `${__dirname}/src/knex/migrations`,
-  //   },
-  //   seeds: {
-  //     directory: `${__dirname}/src/knex/seeds`,
-  //   },
-  // },
   development: {
     client: 'postgresql',
     connection: {
-      host: process.env.DEV_DB_HOST,
-      user: process.env.DEV_DB_USER,
-      database: 'homework-club',
-      password: process.env.DEV_DB_PASS,
-      port: 5432,
+      connectionString: process.env.DATABASE_URL,
+      connectionTimeoutMillis: 5000,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
+      tableName: 'knex_migrations',
       directory: `${__dirname}/src/knex/migrations`,
     },
     seeds: {
