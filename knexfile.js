467b348c8e8931427b603fd92468283c35e03f5a
Nirmeet <nirmeet.baweja@gmail.com>
2021-10-13 22:10:14 +0100

    added developmentLocal environment in knexfile.js
    Added the dev env called developmentLocal to use local postgress DB

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,22 @@
 require('dotenv').config()
 
 module.exports = {
+  developmentLocal: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DEV_DB_HOST,
+      user: process.env.DEV_DB_USER,
+      database: 'homework-club',
+      password: process.env.DEV_DB_PASS,
+      port: 5432,
+    },
+    migrations: {
+      directory: `${__dirname}/src/knex/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/knex/seeds`,
+    },
+  },
   development: {
     client: 'postgresql',
     connection: {
