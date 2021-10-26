20fb08bd0d7b68d453a8e3949c2602f48516671d
Ade <debbyinka21@gmail.com>
2021-10-26 20:33:39 +0100

    Reset password (#8)
    * forgot password and resetting password
    
    * forgot password and reset password functions
    
    * fixed the mailing issues
    
    * cleared the errors
    
    * fixed reset password
    
    * return issues fixed
    
    * persisting return issues resolved
    
    * moved the from email to the environment
    
    * first letter issue resolved
    
    * bugfix:final touches
    
    * bug fix- changed res.send to return statements in services.js
    
    * added more minutes for token expiry
    
    * removed console.logs
    
    * made the route /auth/reset-password/:token as public
    
    Co-authored-by: Nirmeet <nirmeet.baweja@gmail.com>

--- src/routes/auth/controllers.js
+++ src/routes/auth/controllers.js
@@ -41,3 +41,29 @@ export const signIn = async (req, res) => {
     return res.status(401).send({ err: 'Wrong email and/or password.' })
   }
 }
+
+export const forgotPassword = async (req, res) => {
+  try {
+    const result = await services.forgotPassword(req, res)
+    if (result.err) {
+      res.status(500).send(result.err)
+    }
+    return res.status(200).send(result)
+  } catch (err) {
+    console.log(err)
+    return res.status(401).send('Wrong credentials.')
+  }
+}
+
+export const resetPassword = async (req, res) => {
+  try {
+    const result = await services.resetPassword(req, res)
+    if (result.err) {
+      res.status(500).send(result.err)
+    }
+    return res.status(200).send(result)
+  } catch (err) {
+    console.log(err)
+    return res.status(401).send('Wrong credentials.')
+  }
+}
